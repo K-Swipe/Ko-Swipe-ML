4a3741c37519fb8cc893613d6592a15c97316e40
Myungbin <mbgwak1103@gmail.com>
2024-09-10 21:41:39 +0900

    🎨

---
+++ Recommended/location_visit.py
@@ -0,0 +1,14 @@
+"""가장 많이 방문한 지역을 추출하는데 필요한 데이터를 만드는 코드입니다."""
+import pandas as pd
+
+
+train = pd.read_csv(r"C:\workspace\Ko-Swipe-ML\data\preprocessing\관광지 추천시스템 Trainset.csv")
+test = pd.read_csv(r"C:\workspace\Ko-Swipe-ML\data\preprocessing\관광지 추천시스템 Testset.csv")
+
+train = train[["TRAVEL_ID", "VISIT_AREA_NM"]]
+test = test[["TRAVEL_ID", "VISIT_AREA_NM"]]
+
+df = pd.concat([train, test]).reset_index(drop=True)
+
+if __name__ == "__main__":
+    df.to_csv(r"C:\workspace\Ko-Swipe-ML\data\preprocessing\followup_places_.csv", index=False)
